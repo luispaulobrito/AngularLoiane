4435292c3a874af472af73b6a975ffc3167333b4
Luis Paulo <luiss_paullo@hotmail.com>
2023-06-30 15:35:05 -0300

    event biding

--- data-biding/src/app/data-biding/data-biding.component.ts
+++ data-biding/src/app/data-biding/data-biding.component.ts
@@ -10,6 +10,9 @@ export class DataBidingComponent {
   url: string = 'http://loiane.com'
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com.br/400/200/?3';
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
 
   getValor(){
     return 1;
@@ -17,5 +20,20 @@ export class DataBidingComponent {
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert('O botão foi clicado');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = ((<HTMLInputElement>evento.target).value);
+  }
+  salvarValor(valor: string){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 }
