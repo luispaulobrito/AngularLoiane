9980e3ead6978a08e625a4ce4d3b4c6e0454b30c
Luis Paulo <luiss_paullo@hotmail.com>
2023-08-11 10:38:11 -0300

    passando parametros na rota

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
