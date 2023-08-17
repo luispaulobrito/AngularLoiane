aa1082262961035c40fc94a06e3664d2c506bd64
Luis Paulo <luiss_paullo@hotmail.com>
2023-08-17 11:31:08 -0300

    usar interface para desativar guarda de rotas

--- rotas/src/app/alunos/aluno-form/aluno-form.component.ts
+++ rotas/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AlunosService } from '../alunos.service';
+import { IFormCanDeactivate } from 'src/app/guards/iform.candeactivate';
 
 @Component({
   selector: 'app-aluno-form',
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
 })
-export class AlunoFormComponent {
+export class AlunoFormComponent implements IFormCanDeactivate {
   aluno: any;
   inscricao: Subscription = new Subscription;
   private formMudou: boolean = false;
@@ -17,22 +18,23 @@ export class AlunoFormComponent {
     private route: ActivatedRoute,
     private alunosService: AlunosService
   ){}
-
+  
+  
   ngOnInit() {
     this.inscricao = this.route.params.subscribe(
       (params: any) => {
         let id = params ['id']
         this.aluno = this.alunosService.getAluno(id);
-
+        
         if (this.aluno === null) {
           this.aluno = {}
         }
       }
-    );
-  }
-  ngOnDestroy() {
-    this.inscricao.unsubscribe();
-  }
+      );
+    }
+    ngOnDestroy() {
+      this.inscricao.unsubscribe();
+    }
 
   onInput(){
     this.formMudou = true;
@@ -43,5 +45,9 @@ export class AlunoFormComponent {
       confirm('Tem certeza que deseja sair dessa página?');
     }
     return true;
+  }
+  
+  podeDesativar() {
+    return this.podeMudarRota();
   }
 }
--- rotas/src/app/guards/alunos.deactivate.guard.ts
+++ rotas/src/app/guards/alunos.deactivate.guard.ts
@@ -2,17 +2,19 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { AlunoFormComponent } from "../alunos/aluno-form/aluno-form.component";
 import { Observable } from "rxjs";
+import { IFormCanDeactivate } from "./iform.candeactivate";
 
 @Injectable()
-export class AlunosDeactivateGuard implements CanDeactivate<AlunoFormComponent>{
+export class AlunosDeactivateGuard implements CanDeactivate<IFormCanDeactivate>{
 
     canDeactivate(
-        component: AlunoFormComponent, 
+        component: IFormCanDeactivate, 
         currentRoute: ActivatedRouteSnapshot, 
         currentState: RouterStateSnapshot, 
         nextState: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
             console.log('desativacao');
             
-            return component.podeMudarRota();   
+            // return component.podeMudarRota();   
+            return component.podeDesativar();
     }
 }
